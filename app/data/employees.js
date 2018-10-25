3ebb6c906f7f86a7c17ed0d4cfd7906de2e86e7e
ZacSol <42217556+ZacSol@users.noreply.github.com>
2018-10-25 09:13:20 -0500

    modified questions and results

--- app/data/employees.js
+++ app/data/employees.js
@@ -2,31 +2,31 @@ module.exports = [
     { 
         name: "Magneto", 
         photo: "https://vignette.wikia.nocookie.net/xmenmovies/images/3/37/Erik2.jpg/revision/latest/scale-to-width-down/185?cb=20120109095740", 
-        scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] 
+        scores: ["5", "2", "5", "5", "3", "4", "5", "5", "1", "5"] 
     }, 
     { 
         name: "Wolverine", 
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Wolverine_AKA_James_%22Logan%22_Howlett.png/170px-Wolverine_AKA_James_%22Logan%22_Howlett.png", 
-        scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] 
+        scores: ["2", "3", "2", "1", "1", "5", "5", "1", "1", "5"] 
     }, 
     { 
         name: "Professor X", 
         photo: "https://s3-us-west-1.amazonaws.com/prohbtd/uploads/public/58d/045/ec9/58d045ec9e12d387604707.jpg", 
-        scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] 
+        scores: ["5", "5", "5", "5", "5", "1", "1", "1", "5", "5"] 
     }, 
     { 
         name: "Cyclops", 
         photo: "https://qph.fs.quoracdn.net/main-qimg-8d98760fed3dabbe3bafb822872c98df", 
-        scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] 
+        scores: ["4", "4", "4", "3", "2", "5", "3", "2", "3", "3"] 
     }, 
     { 
         name: "Beast", 
         photo: "https://vignette.wikia.nocookie.net/jadensadventures/images/1/14/X-Men-Movies-Beast-Comparison-Hoult-Grammer.jpg/revision/latest?cb=20140409001401", 
-        scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] 
+        scores: ["5", "2", "2", "4", "5", "3", "3", "1", "5", "5"] 
     }, 
     { 
         name: "Jean Grey", 
         photo: "https://vignette.wikia.nocookie.net/xmenmovies/images/d/d9/Xc_%282%29.jpg/revision/latest?cb=20140723225150", 
-        scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] 
+        scores: ["4", "3", "3", "3", "2", "1", "2", "2", "3", "4"] 
     }
 ]
